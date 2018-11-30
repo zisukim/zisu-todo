e0a65ab60fba8e0dfb2ae6e65f751ad93ec67e3d
zisu <zisu.kim@nhnent.com>
2018-11-30 13:45:56 +0900

    Update todos

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, TextInput, ScrollView, AsyncStorage } from 'react-native';
 import ToDo from './ToDo';
 import { AppLoading } from 'expo';
 import uuidv1 from 'uuid/v1'
@@ -9,14 +9,15 @@ const { height, width } = Dimensions.get("window")
 export default class App extends React.Component {
   state = {
     newToDo: '',
-    loadedToDos: false
+    loadedToDos: false,
+    toDos: {}
   };
   componentDidMount = () => {
     this._loadToDos();
 
   };
   render() {
-    const { newToDo, loadedToDos } = this.state;
+    const { newToDo, loadedToDos, toDos } = this.state;
     if (!loadedToDos) {
       return <AppLoading />
 
@@ -38,7 +39,14 @@ export default class App extends React.Component {
             onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
-            <ToDo text={"Hello ~~~"} />
+            {Object.values(toDos).reverse().map(toDo =>
+              <ToDo key={toDo.id} {...toDo}
+                deleteToDo={this._deleteToDo}
+                completeToDo={this._completeToDo}
+                uncompleteToDo={this._uncompleteToDo} 
+                updateToDo={this._updateToDo}
+                />
+            )}
           </ScrollView>
         </View>
       </View>
@@ -49,16 +57,25 @@ export default class App extends React.Component {
       newToDo: text
     })
   };
-  _loadToDos = () => {
-    this.setState({
-      loadedToDos: true
+  _loadToDos = async () => { // why?
+    try {
+      const toDos = await AsyncStorage.getItem("toDos");
+      const parsedToDos = JSON.parse(toDos);
+      this.setState({
+        loadedToDos: true,
+        toDos: parsedToDos
+      })
 
-    })
+    } catch (err) {
+      console.log("err", err);
+
+    }
+    
   };
   _addToDo = () => {
     const { newToDo } = this.state;
     if (newToDo !== '') {
-      this.setState( prevState => {
+      this.setState(prevState => {
         const ID = uuidv1();
         const newToDoObject = {
           [ID]: {
@@ -67,14 +84,91 @@ export default class App extends React.Component {
             text: newToDo,
             createAt: Date.now()
           }
+        };
+        const newState = {
+          ...prevState,
+          newToDo: '',
+          toDos: {
+            ...prevState.toDos,
+            ...newToDoObject
 
-        }
+          }
+        };
+        this._saveToDo(newState.toDos);
+        return { ...newState };
 
       });
 
     }
 
   };
+  _deleteToDo = id => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos;
+      delete toDos[id];
+      const newState = {
+        ...prevState,
+        ...toDos
+      };
+      this._saveToDo(newState.toDos);
+      return { ...newState };
+
+    });
+
+  };
+  _uncompleteToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: false
+          }
+        }
+      }
+      this._saveToDo(newState.toDos);
+      return { ...newState };
+    })
+  };
+  _completeToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: true
+          }
+        }
+      }
+      this._saveToDo(newState.toDos);
+      return { ...newState };
+    })
+  };
+  _updateToDo = (id, text) => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            text: text
+          }
+        }
+      }
+      this._saveToDo(newState.toDos);
+      return { ...newState };
+    })
+  };
+  _saveToDo = newToDos => {
+    const saveToDos = AsyncStorage.setItem("toDos", JSON.stringify(newToDos));
+    // AsyncStorage can save only strings.
+
+  }
 }
 
 const styles = StyleSheet.create({
--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native";
+import PropTypes from "prop-types";
 
 const { width, height } = Dimensions.get("window")
 
 export default class ToDo extends Component { //stateful component
-    state = {
-        isEditing: false,
-        isCompleted: false,
-        toDoValue: ""
+    constructor(props) {
+        super(props);
+        this.state = { isEditing: false, toDoValue: props.text }
     };
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        isCompleted: PropTypes.bool.isRequired,
+        deleteToDo: PropTypes.func.isRequired,
+        completeToDo: PropTypes.func.isRequired,
+        uncompleteToDo: PropTypes.func.isRequired,
+        updateToDo: PropTypes.func.isRequired,
+        id: PropTypes.string.isRequired
+    };
+    
     render() {
-        const { isCompleted, isEditing, toDoValue } = this.state;
-        const { text } = this.props;
+        const { isEditing, toDoValue } = this.state;
+        const { text, id, deleteToDo, isCompleted } = this.props;
         return (
             <View style={styles.container}>
                 <View style={styles.column}>
@@ -46,7 +56,7 @@ export default class ToDo extends Component { //stateful component
                                 <Text style={styles.actionText}>✏️️️</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPressOut={(event) => {event.stopPropagation; deleteToDo(id)}}>
                             <View style={styles.actionContainer}>
                                 <Text style={styles.actionText}>❌</Text>
                             </View>
@@ -57,21 +67,29 @@ export default class ToDo extends Component { //stateful component
             </View>
         )
     };
-    _toggleComplete = () => {
-        this.setState(prevState => {
-            return {
-                isCompleted: !prevState.isCompleted
-            }
-        });
+    _toggleComplete = (event) => {
+        event.stopPropagation();
+        const { isCompleted, id, completeToDo, uncompleteToDo } = this.props;
+        if (isCompleted) {
+            uncompleteToDo(id);
+
+        } else {
+            completeToDo(id);
+
+        }
     };
-    _startEditing = () => {
-        const { text } = this.props;
+    _startEditing = (event) => {
+        event.stopPropagation();
         this.setState({
-            isEditing: true,
-            toDoValue: text
+            isEditing: true
         });
     };
-    _finishEditing = () => {
+    _finishEditing = (event) => {
+        event.stopPropagation();
+        const { toDoValue } = this.state;
+        const { id, updateToDo } = this.props;
+        updateToDo(id, toDoValue);
+
         this.setState({
             isEditing: false
         })
@@ -136,8 +154,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 10
     },
     input: {
-        marginVertical: 20,
-        color: '#bbb'
+        color: '#4c4c4c'
     }
 
 })
